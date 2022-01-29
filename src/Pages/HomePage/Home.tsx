61e408011ce8d676c0d319d8e4f4c2e4e9b42d17
SoEasy2 <soeasy24@mail.ru>
2022-01-30 02:07:30 +0300

    fixed

--- src/Pages/HomePage/Home.tsx
+++ src/Pages/HomePage/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="myBody">
             <div className="logoWrap">
                 <svg version="1.1" id="Слой_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 855 150" xmlSpace="preserve">
-                    <style type="text/css" dangerouslySetInnerHTML={{__html: "\n\t.st0{fill:none;stroke:#FFFFFF;stroke-width:2;stroke-miterlimit:10;}\n\t.st1{font-family:'open sans';}\n\t.st2{font-size:80px;}\n" }} />
+                    <style type="text/css" dangerouslySetInnerHTML={{__html: "\n\t.st0{fill:none;stroke:#FFFFFF;stroke-width:2;stroke-miterlimit:10;}\n\t.st1{font-family:'open sans';}\n\t.st2{font-size:63px;}\n" }} />
                     <text transform="matrix(1 0 0 1 0 125.5508)" className="st0 st1 st2">Mutant Shiba Club</text>
                 </svg>
                 <div className="wrapLink">
